2e706d885c4656194953d7f1c6af4f87483fe663
AlexanderBowers <7319abowers@gmail.com>
2021-02-18 09:06:27 -0800

    arbitrary commit

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,5 +1,5 @@
 class Playlist < ApplicationRecord
   belongs_to :user
-  has_many :videos
+  has_many :videos, dependent: :destroy
   has_many :likes, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   resources :playlists do
     resources :likes
   end
-  resources :likes
   resources :videos
   namespace :api do
     namespace :v1 do
